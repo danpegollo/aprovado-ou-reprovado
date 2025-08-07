6333959e3ad6b6dda2f27336e30bd62b21aa6d3e
danpegollo <danielpegollo29@gmail.com>
2025-08-07 15:44:17 -0300

    falha return app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function calcularMedia() {
   if (notaInvalida) {
     document.querySelector("#resposta").innerHTML =
       "Por favor, insira apenas notas entre 0 e 10.";
-  }
+  return }
   (() => {
     const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
